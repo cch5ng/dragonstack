0ce2f2ba4ed01ac2a0a62c707c1ae5779c5156be
Carol Chung <cch5ng@gmail.com>
2018-12-09 21:24:34 -0800

    add endpoint for generation

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const GenerationEngine = require('./generation/engine');
 const dragonRouter = require('./api/dragon');
+const generationRouter = require('./api/generation');
 
 const app = express();
 const engine = new GenerationEngine();
@@ -9,6 +10,7 @@ const engine = new GenerationEngine();
 app.locals.engine = engine;
 
 app.use('/dragon', dragonRouter);
+app.use('./generation', generationRouter);
 
 engine.start();
 
