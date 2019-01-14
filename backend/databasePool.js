340bbd8816f1def46eea600c0462fcdeac79a033
Carol Chung <cch5ng@gmail.com>
2019-01-13 17:37:11 -0800

    use database pool setup

--- backend/databasePool.js
+++ backend/databasePool.js
@@ -2,3 +2,12 @@ const { Pool } = require('pg');
 const databaseConfiguration = require('./secrets/databaseConfiguration');
 
 const pool = new Pool(databaseConfiguration);
+
+module.exports = pool;
+
+// test pool connection
+// pool.query('SELECT * from generation', (err, resp) => {
+// 	if (err) return console.log('error', err);
+
+// 	console.log('response.rows', resp.rows);
+// })
