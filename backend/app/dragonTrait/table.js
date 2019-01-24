bcfa8b509f45b37e71ce17eacac65021011bcb42
Carol Chung <cch5ng@gmail.com>
2019-01-23 18:46:07 -0800

    more updates storeDragonTrait()

--- backend/app/dragonTrait/table.js
+++ backend/app/dragonTrait/table.js
@@ -3,18 +3,20 @@ const TraitTable = require('../trait/table');
 
 class DragonTraitTable {
 	static storeDragonTrait(dragonId, traitType, traitValue) {
-		// TraitTable.getTraitId()
-		// pool.query(
-		// 	`INSERT INTO dragonTrait("dragonId", "traitId")
-		//		VALUES($1, $2)`,
-		//	[dragonId, traidId],
-		//	(error, response) => {
-		//		if (error) console.error(error);
+		TraitTable.getTraitId({ traitType, traitValue })
+			.then(({ traitId }) => {
+				pool.query(
+					`INSERT INTO dragonTrait("dragonId", "traitId")
+						VALUES($1, $2)`,
+					[dragonId, traitId],
+					(error, response) => {
+						if (error) console.error(error);
 
-		//			
-		//	}
-		//)
 
+					}
+				)				
+			})
+			.catch(error => console.error('error', error))
 
 	}
 }
